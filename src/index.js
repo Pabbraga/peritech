2460386585fa959250e2c026f2d6b0a654c7b676
Pabbraga <pablofelix567@gmail.com>
2023-10-17 19:35:42 -0300

    style: add font-family

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Karla:wght@400;600;700&display=swap');
+</style>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
