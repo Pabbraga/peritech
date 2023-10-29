4a45fd31407d19174c3f1781cd83fbc152e1cb6d
Pabbraga <pablofelix567@gmail.com>
2023-10-29 20:28:31 -0300

    feat: add routing feature

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Store from './pages/Store';
 import App from './App';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Store />
+      },
+      {
+        path: "page/:pageNumber",
+        element: <Store />
+      },
+      {
+        path: "category/:category",
+        element: <Store />
+      }
+    ]
+  },
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>
 );
