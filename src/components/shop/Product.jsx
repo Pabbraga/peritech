a455b8e1f3afc5b752f731eb502a905e309fe81b
Pabbraga <pablofelix567@gmail.com>
2023-10-29 20:37:25 -0300

    refactor: add key value in product

--- src/components/shop/Product.jsx
+++ src/components/shop/Product.jsx
@@ -1,8 +1,8 @@
 import styles from './Product.module.css';
 
-function Product({image, name, price}) {
+function Product({id, image, name, price}) {
     return (
-        <div className={styles.product_container}>
+        <li className={styles.product_container} key={id}>
             <img src={image} className={styles.image} alt={name} />
             <div className={styles.name}>
                 <p>{name}</p>
@@ -10,7 +10,7 @@ function Product({image, name, price}) {
             <div className={styles.price}>
                 <p>R$ {price}</p>
             </div>
-        </div>
+        </li>
     )
 }
 
