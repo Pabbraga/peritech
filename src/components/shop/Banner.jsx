9a066779864eccc26ad6f055e4e53ee632a0db82
Pabbraga <pablofelix567@gmail.com>
2023-10-29 20:42:33 -0300

    feat: create banner component

---
+++ src/components/shop/Banner.jsx
@@ -0,0 +1,11 @@
+import styles from './Banner.module.css';
+
+function Banner() {
+    return(
+        <div className={styles.banner_container}>
+
+        </div>
+    )
+}
+
+export default Banner;
