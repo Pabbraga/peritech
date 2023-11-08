148cf5db83ac3c42a64dd8ab13db23a4743f0d97
Pabbraga <pablofelix567@gmail.com>
2023-11-08 20:59:43 -0300

    feat: set context feature in shopping cart

--- src/components/hud/ShoppingCart.jsx
+++ src/components/hud/ShoppingCart.jsx
@@ -1,7 +1,11 @@
+import CartItem from './CartItem';
 import styles from './ShoppingCart.module.css';
 
+import { useShoppingCart } from '../../context/ShoppingCartContext';
+
 function ShoppingCart() {
-    const isEmpty = true;
+    const { cartItems } = useShoppingCart();
+    const isEmpty = cartItems?false:true;
 
     return(
         <div className={styles.shopping_cart}>
@@ -9,6 +13,13 @@ function ShoppingCart() {
                 <p>Meu Carrinho</p>
             </div>
             <div className={styles.list}>
+                {!isEmpty && cartItems.map(item => (
+                    <CartItem 
+                        key={item.id}
+                        {...item}
+                    />
+                ))
+                }
                 {isEmpty && <div className={`${styles.side_to_side} ${styles.msg}`}><p>Seu carrinho está vazio!</p></div>}
             </div>
             <div className={`${styles.side_to_side} ${styles.finish}`}>
