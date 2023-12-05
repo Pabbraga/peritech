e1c54d7c50ed7777c60e80fa338c634cac1d3fb1
Pabbraga <pablofelix567@gmail.com>
2023-12-05 16:16:13 -0300

    feat: add redirect page to start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return;
+}
+
 function App() {
   return (
     <ShoppingCartProvider>
+      <ScrollToTop/>
       <Header />
         <Outlet />
       <Footer />
