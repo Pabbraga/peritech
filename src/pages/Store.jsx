924316572b5595272ab373237cfe2e821dca3162
Pabbraga <pablofelix567@gmail.com>
2024-01-25 20:46:37 -0300

    fix: resolve problem when trying to access a page

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,4 +1,5 @@
-import { useParams, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import styles from './Store.module.css';
 import Banner from '../components/storeComponents/Banner';
@@ -7,37 +8,30 @@ import Product from '../components/storeComponents/Product';
 import storeProducts from '../data/products.json';
 import Navbar from '../components/layout/Navbar';
 import NotFound from './NotFound';
-import { useEffect, useState } from 'react';
 
 function Store() {
-    const { pathname } = useLocation();
     const [err, setErr] = useState(false);
     let { pageNumber, category } = useParams();
-
-    useEffect(() => {
-        const handleErr = () => {
-            if(pathname === "/") {
-                setErr(false);
-                return;
-            }
-            if(!storeProducts.find(e => e.category === category)) {
-                setErr(true);
-                return;
-            }
-            setErr(false);
-        }
-        handleErr();
-    })
 
     const loadProducts = (page = 1, category = null) => {
         const firstItem = (page > 1 ? page * 16 / 2 : 0);
         const lastItem = page * 16;
+        let items = storeProducts;
+        let qntItems = 0;
 
         if(category) {
-            return storeProducts.filter(product => product.category === category);
+            items = items.filter(product => product.category === category);
         }
 
-        return storeProducts.slice(firstItem, lastItem);
+        items = items.slice(firstItem, lastItem);
+
+        qntItems = Object.keys(items).length;
+
+        if(qntItems < 1) {
+            setErr(true);
+        }
+
+        return items;
     }
 
     return(
